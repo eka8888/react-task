0a18036cdd8a22b328d44c217cbf144a3ee01e83
eka8888 <77500720+eka8888@users.noreply.github.com>
2022-10-21 10:05:42 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import PostsList from './components/PostsList';
+import Header from './components/Header';
+import Footer from './components/Footer'
+
+function App() {
+    return ( <div>
+         <Header/>
+        <PostsList/>
+        <Footer/>
+        </div>
+    );
+}
+
+export default App;
